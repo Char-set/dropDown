9b6c3c6390efaf09505e5200d0564e92048968db
Char-set <xcharset@163.com>
2017-04-13 11:33:19 +0800

    Update vue-drop-down.vue

--- vue-drop-down.vue
+++ vue-drop-down.vue
@@ -4,7 +4,7 @@
  *
  * @param dropDown 下拉 刷新过程中需要执行的函数
  * @param stop 下拉刷新停止的标识，组件会watch
- * @param top 组件的style top值
+ * @param top 组件的style margin-top值 可以为负数
  *
  * 例子:
  * <ui-drop-down :on-pull-down="dropDown" :stop.sync="stopDropDown" :top="'0'">
